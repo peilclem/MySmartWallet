ae0509eeea7326769216acb1c436c9859ed830e3
DataPyPeil <peillon.clement74@gmail.com>
2026-07-27 17:04:26 +0200

    [CHG] Modify database to remove useless date table

--- src/mysmartwallet/models/database/create_database.py
+++ src/mysmartwallet/models/database/create_database.py
@@ -1,25 +1,16 @@
-import os
 import sqlite3
 
 ROOT_DIR = r"C:/Users/peill/Documents/Python_Scripts/MySmartWallet"
+DB_PATH = ROOT_DIR + "data/MySmartWallet.db"
 
 def create_database():
     """
     Create tables of the database
     """
-    con = sqlite3.connect(ROOT_DIR / "data/MySmartWallet.db")
+    con = sqlite3.connect(DB_PATH)
     cursor = con.cursor()
     
     # Tables de dimension
-    cursor.execute('''
-                   CREATE TABLE IF NOT EXISTS D_Date
-                   (Date_ID INTEGER PRIMARY KEY, --YYYYMM
-                    Month TEXT NOT NULL,
-                    Quarter TEXT NOT NULL,
-                    Year INTEGER NOT NULL
-                    )
-    ''')
-    
     cursor.execute('''
                    CREATE TABLE IF NOT EXISTS D_Bank
                    (Wallet_ID TEXT PRIMARY KEY,
@@ -28,19 +19,19 @@ def create_database():
                     )
     ''')
     
-    cursor.execute('''
-                    CREATE TABLE IF NOT EXISTS D_Balance
-                    (Balance_ID TEXT PRIMARY KEY,
-                    Username TEXT NOT NULL,
-                    Date_ID INTEGER NOT NULL,
-                    Wallet_ID TEXT NOT NULL,
-                    Solde_IN REAL NOT NULL,
-                    Solde_OUT REAL NOT NULL,
-                    FOREIGN KEY (Date_ID) REFERENCES D_Date(Date_ID),
-                    FOREIGN KEY (Wallet_ID) REFERENCES D_Bank(Wallet_ID),
-                    UNIQUE (Username, Date_ID, Wallet_ID)
-                    )
-    ''')
+    # cursor.execute('''
+    #                 CREATE TABLE IF NOT EXISTS D_Balance
+    #                 (Balance_ID TEXT PRIMARY KEY,
+    #                 Username TEXT NOT NULL,
+    #                 Date_ID INTEGER NOT NULL,
+    #                 Wallet_ID TEXT NOT NULL,
+    #                 Solde_IN REAL NOT NULL,
+    #                 Solde_OUT REAL NOT NULL,
+    #                 FOREIGN KEY (Date_ID) REFERENCES D_Date(Date_ID),
+    #                 FOREIGN KEY (Wallet_ID) REFERENCES D_Bank(Wallet_ID),
+    #                 UNIQUE (Username, Date_ID, Wallet_ID)
+    #                 )
+    # ''')
     
     # Table de fait
     cursor.execute('''
@@ -49,9 +40,9 @@ def create_database():
                     Username TEXT NOT NULL,
                     Date_ID INTEGER NOT NULL,
                     Wallet_ID TEXT NOT NULL,
-                    Libelle TEXT NOT NULL,
-                    Montant REAL NOT NULL,
-                    Categorie TEXT NOT NULL,
+                    Label TEXT NOT NULL,
+                    Amount REAL NOT NULL,
+                    Category TEXT NOT NULL,
                     FOREIGN KEY (Date_ID) REFERENCES D_Date(Date_ID),
                     FOREIGN KEY (Wallet_ID) REFERENCES D_Bank(Wallet_ID)
                     )
@@ -62,10 +53,3 @@ def create_database():
     
     return con
 
-def connect_database():
-    """ Connect to database (or creeate if not exists) """
-    if "MySmartWallet.db" not in os.listdir(ROOT_DIR / "data"):
-        con = create_database()
-    else:
-        con = sqlite3.connect(ROOT_DIR / "data/MySmartWallet.db")
-    return con
