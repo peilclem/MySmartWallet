0127fa8a24ed63b3f7a8c3323a2299753c09eaae
DataPyPeil <peillon.clement74@gmail.com>
2026-08-01 23:06:05 +0200

    [ADD] Account repository to handle connection with user table

---
+++ src/mysmartwallet/database/AccountRepository.py
@@ -0,0 +1,44 @@
+from mysmartwallet.database.DatabaseManager import DatabaseManager
+
+class AccountRepository:
+    def __init__(self, db: DatabaseManager):
+        self.db = db
+
+    def add(self, user_id, bank_id, account_type):
+        account_exists = self.check_account_existence(user_id, bank_id, account_type)
+
+        if account_exists:
+            return
+        
+        query = """
+        INSERT INTO Accounts
+        (User_id, Bank_ID, Type)
+        VALUES (?, ?, ?)
+        """
+
+        self.db.execute(
+            query,
+            (
+                user_id,
+                bank_id,
+                account_type
+            )
+        )
+
+        self.db.commit()
+
+    def check_account_existence(self, user_id, bank_id, account_type):
+        query = """
+        SELECT * FROM Accounts
+        WHERE User_id = ? AND Bank_ID = ? AND Type = ?
+        """
+
+        cursor = self.db.execute(query, (user_id, bank_id, account_type))
+        result = cursor.fetchone()
+
+        return result is not None
+
+    def fetch_all(self):
+        query = """SELECT * FROM Accounts"""
+        rows = self.db.fetch_all(query)
+        return rows
